7e6d4b2451cfcadd1bc8d5c65c26f9c03c761bca
hogucc <uku.h1r8@gmail.com>
2023-10-08 15:43:50 +0900

    Display the selected range in a message box when a word is selected

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,16 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('ruby-constant-vision.helloWorld', () => {
-		const editor = vscode.window.activeTextEditor;
+	vscode.window.onDidChangeTextEditorSelection(e => {
+		const editor = e.textEditor;
+		const document = editor.document;
+		const selection = editor.selection;
+		const word = document.getText(selection);
 
-		if (editor) {
-			const document = editor.document;
-			const selection = editor.selection;
-			const word = document.getText(selection);
-
+		if (!selection.isEmpty) {
 			vscode.window.showInformationMessage('Selected word: ' + word);
 		}
 	});
-
-	context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
